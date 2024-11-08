6b7790a3e4e3986541786acb0800b76a606c6a6f
wilson <wilsonliu202@gmail.com>
2024-11-08 17:22:53 -0500

    route landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Chatbot from "./pages/ChatBot";
+import LandingPage from "./pages/landingpage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/chat" element={<Chatbot />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
